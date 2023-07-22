bcbd2a662dbf6373e30738f630b6a272b297bbe3
iam-dj <6887240+iam-dj@users.noreply.github.com>
2023-07-22 14:07:16 -0700

    updates to resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
 import resume from '../assets/res1.png'
 import resume2 from '../assets/res2.png'
+import '../../components/index.css';
+import Dexter_Resume from '../assets/Dexter_Resume.pdf'
 
 export default function Resume() {
 const color = 
@@ -8,8 +11,41 @@ const color =
     backgroundColor:"#f4f4f4"
   };
 
+const btn = 
+  {
+    backgroundColor:"pink!important",
+  };
+
+
+ class DownloadButton extends Component {
+  handleDownload = () => {
+    const pdfFilePath = Dexter_Resume;
+
+    // Create a temporary anchor element and set its attributes.
+    const link = document.createElement('a');
+    link.href = pdfFilePath;
+    link.download = 'Dexter_Resume.pdf'; // Set the downloaded file's name.
+
+    // Append the link to the body and click it programmatically to trigger the download.
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up: Remove the temporary link from the DOM.
+    document.body.removeChild(link);
+  };
+
+  render() {
+    return <butto className='PDFbtn' onClick={this.handleDownload}>Download</butto>;
+  }
+}
+
+
   return (
     <div style={color} className='col-12'>
+      <DownloadButton 
+      // style={btn} 
+      className='PDFbtn'
+        />
       <img className='mainResume' src={resume} alt='dexter's resume/>
       <img className='mainResume' src={resume2} alt='dexter's resume/>
     </div>
