e81bc9e8ffd40112bda2ca46dee9e5307ae834e6
iam-dj <6887240+iam-dj@users.noreply.github.com>
2023-07-27 12:09:16 -0700

    updates

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -23,7 +23,7 @@ export default function About() {
             <div className="portada"></div>
             <div className="title-total">
               <div className="title">Full Stack Developer & Executive Digital Strategist</div>
-              <h2>Dexter "DJ" Jules</h2>
+              <h2>Dexter 'DJ' Jules</h2>
 
               <div className="desc">
               An unyielding drive to innovate propels my career in full-stack development and digital marketing strategy. My portfolio is comprised of an assortment of successful projects from diverse industries, highlighting my ability to deliver thought-provoking graphics, cutting-edge web applications, and captivating brand narratives. I offer unique, comprehensive solutions that drive unparalleled business growth. My robust understanding of the most recent developments within the industry, coupled with cutting-edge technological expertise, has enabled me to successfully develop and execute plans that produce substantial results across numerous demographics utilizing diverse communication methods.
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import React from 'react';
-import resume from '../assets/res1.png'
-import resume2 from '../assets/res2.png'
-import '../../components/index.css';
-import Dexter_Resume from '../assets/Dexter_Resume.pdf'
+import resume from "../assets/res1.png";
+import resume2 from "../assets/res2.png";
+import "../../components/index.css";
+import Dexter_Resume from "../assets/Dexter_Resume.pdf";
 
 export default function Resume() {
-const color = 
-  {
-    backgroundColor:"#f4f4f4"
+  const color = {
+    backgroundColor: "#f4f4f4",
   };
 
-const btn = 
-  {
-    backgroundColor:"pink!important",
+  const btn = {
+    backgroundColor: "pink!important",
   };
-
 
   function handleDownload() {
     const pdfFilePath = Dexter_Resume;
 
     // Create a temporary anchor element and set its attributes.
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = pdfFilePath;
-    link.download = 'Dexter_Resume.pdf'; // Set the downloaded file's name.
-
+    link.download = "Dexter_Resume.pdf"; // Set the downloaded file's name.
 
     // Append the link to the body and click it programmatically to trigger the download.
     document.body.appendChild(link);
@@ -34,13 +30,13 @@ const btn =
     document.body.removeChild(link);
   }
 
- 
-
   return (
-    <div style={color} className='col-12'>
-<button className='PDFbtn' onClick={handleDownload}>Download</button>
-      <img className='mainResume' src={resume} alt='dexter's resume/>
-      <img className='mainResume' src={resume2} alt='dexter's resume/>
+    <div style={color} className="col-12">
+      <button className="PDFbtn" onClick={handleDownload}>
+        Download Resume
+      </button>
+      <img className="mainResume" src={resume} alt="dexter" s resume />
+      <img className="mainResume" src={resume2} alt="dexter" s resume />
     </div>
   );
 }
